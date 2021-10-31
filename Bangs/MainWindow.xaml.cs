9cb94c26ef63933c8487733b7d04e004ce8e172c
CC Liu <Y-Y-Liu@hotmail.com>
2021-10-31 18:18:39 +0800

    添加项目文件。

---
+++ Bangs/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bangs
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {   /// <summary> 
+        /// 设置当前窗体位置
+        /// </summary> 
+        /// <returns></returns> 
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+
+        /// <summary> 
+        /// 得到当前活动的窗口 
+        /// </summary> 
+        /// <returns></returns> 
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern System.IntPtr GetForegroundWindow();
+
+        /// <summary>   
+        /// 设置鼠标的坐标   
+        /// </summary>   
+        /// <param name="x">横坐标</param>   
+        /// <param name="y">纵坐标</param>   
+        [DllImport("User32")]
+        public extern static void SetCursorPos(int x, int y);
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+        }
+
+        /// <summary>   
+        /// 获取鼠标的坐标   
+        /// </summary>   
+        /// <param name="lpPoint">传址参数，坐标point类型</param>   
+        /// <returns>获取成功返回真</returns>   
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetCursorPos(out POINT pt);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.Manual;
+
+            Left = (SystemParameters.PrimaryScreenWidth - 250) / 2;
+            Top = -10;
+            ShowInTaskbar = false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetWindowPos(new WindowInteropHelper(this).Handle, -1, 0, 0, 0, 0, 1 | 2);
+
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            GetCursorPos(out var x);
+            SetCursorPos(x.X, (int)(Height + Top));
+        }
+    }
+}
